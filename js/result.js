483cca9d1a7be28275e3ec45f15e6b205d0c6abb
Javier <javierondicol84@gmail.com>
2024-07-17 11:49:33 -0300

    doing search results button

--- js/result.js
+++ js/result.js
@@ -2,13 +2,12 @@
 
 
 window.onload = function() {
-    const search = localStorage.getItem('search');
+    const params = new URLSearchParams(window.location.search);
+    const search = params.get('search'); //usar search para buscar un producto con like de esto
 
     document.querySelector("#test").innerHTML = search;
 
-    
 
 
 
-    localStorage.removeItem('search');
 }
--- js/search.js
+++ js/search.js
@@ -4,12 +4,17 @@ document.querySelector("#searchButton").addEventListener("click", searchResult);
 
 
 async function searchResult(e) {
+    
     e.preventDefault();
-  
     let searchValue = document.querySelector("#searchValue").value;
+
+    
+     /*
+
     localStorage.setItem('search', searchValue);
-    window.location.href = `pages/result.html`;
+    window.location.href = `pages/result.html`;*/
 
-
+    
+    window.location.href = `pages/result.html?search=${encodeURIComponent(searchValue)}`;
     
 }
