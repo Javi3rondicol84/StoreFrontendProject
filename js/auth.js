42c898041d9196aa025ce7b43240a251f744dad8
Javier <javierondicol84@gmail.com>
2024-08-02 09:33:12 -0300

    expired token added

--- js/auth.js
+++ js/auth.js
@@ -6,19 +6,29 @@ async function main() {
     let isLogged = document.querySelector("#logged");
     let cart = document.querySelector(".cart");
     let createAccount = document.querySelector(".createAccount");
-    const decodedToken = jwt_decode(token);
 
     if(token) {
-        createAccount.classList.add("hidden");
-        cart.classList.remove("hidden");
-        isLogged.innerHTML = "Cerrar Sesión ["+decodedToken.sub+":"+decodedToken.authorities+"]";
-    }
-    else {
+        const decodedToken = jwt_decode(token);
+        const currentTime = Math.floor(Date.now() / 1000); // current time in seconds
+        console.log(token);
+        if (decodedToken.exp > currentTime) {
+            // Token is valid
+            createAccount.classList.add("hidden");
+            cart.classList.remove("hidden");
+            isLogged.innerHTML = "Cerrar Sesión ["+decodedToken.sub+":"+decodedToken.authorities+"]";
+        } else {
+            // Token has expired
+            localStorage.removeItem('token');
+            createAccount.classList.remove("hidden");
+            cart.classList.add("hidden");
+            isLogged.innerHTML = "Iniciar Sesion";
+        }
+    } else {
+        // No token present
         createAccount.classList.remove("hidden");
         cart.classList.add("hidden");
         isLogged.innerHTML = "Iniciar Sesion";
     }
-
 }
 
 main();
